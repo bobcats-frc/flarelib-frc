af8216f432b2e077b5e626481e912cfbdd1b5935
aregulardev <salcandoruk@gmail.com>
2026-03-02 20:39:56 +0300

    Initial release

---
+++ README.md
@@ -0,0 +1,2 @@
+# flarelib-frc
+An FRC robotics utility library, maintained by the Bobcats #11010 software team.
---
+++ src/main/java/frc/robot/constants/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot.constants;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "FlareLib";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = -1;
+  public static final String GIT_SHA = "UNKNOWN";
+  public static final String GIT_DATE = "UNKNOWN";
+  public static final String GIT_BRANCH = "UNKNOWN";
+  public static final String BUILD_DATE = "2026-03-02 20:36:55 GMT+03:00";
+  public static final long BUILD_UNIX_TIME = 1772473015342L;
+  public static final int DIRTY = 129;
+
+  private BuildConstants(){}
+}
